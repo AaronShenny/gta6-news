a5003df3839712677fa325fbe9fd5742872914ea
aaronshenny <aaronshenny102.go@gmail.com>
2026-02-14 08:53:21 +0530

    updated fetch.py

--- backend/fetch.py
+++ backend/fetch.py
@@ -23,7 +23,7 @@ client = None
 if GEMINI_API_KEY:
     client = genai.Client(
     api_key=GEMINI_API_KEY,
-    http_options={"api_version": "v1"}
+    
     
     )
     print(client.models.list())
